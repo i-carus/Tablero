0d467c404b778cb10f7c14c06fa337e7f608717e
i-carus <icarus@lablogoteca.com>
2014-01-15 16:35:42 -0500

    Tablero written as a jquery plugin.

--- Scripts/Tablero.js
+++ Scripts/Tablero.js
@@ -1,143 +1,214 @@
-﻿function Tablero(canvasId) {
-    var mouseDown = false;
-    var canvas = null;
-    var context = null;
-    var thisCanvasId = canvasId;
+﻿//function Tablero(canvasId) {
+//    var mouseDown = false;
+//    var canvas = null;
+//    var context = null;
+//    var thisCanvasId = canvasId;
+
+//    var startPaint = function (x, y) {
+//        mouseDown = true;
+//        canvas = document.getElementById(thisCanvasId);
+//        context = canvas.getContext("2d");
+//        context.lineWidth = 1;
+//        context.beginPath();
+//        context.moveTo(x, y);
+//    };
+
+//    var endPaint = function () {
+//        mouseDown = false;
+//    };
+
+//    var draw = function (x1, y1) {
+//        var x = Math.floor(x1) + 0.5;
+//        var y = Math.floor(y1) + 0.5;
+//        context.lineTo(x, y);
+
+//        if (mouseDown)
+//            context.stroke();
+//    };
+
+//    var init = function () {
+
+//        canvas = document.getElementById(thisCanvasId);
+
+//        context = canvas.getContext("2d");
+
+//        if (context)
+//            context.strokeStyle = "White";
+
+//        hookEvents();
+
+//    };
+
+//    this.startPaint = function(x, y) {
+//        startPaint(x, y);
+//    };
+
+//    this.endPaint = function () {
+//        endPaint();
+//    };
+
+//    this.draw = function (x, y) {
+//        draw(x, y);
+//    };
+
+//    var hookEvents = function () {
+//        $(document).on('mousedown touchstart', '#' + thisCanvasId, function (e) {
+//            var event = window.event;
+//            var x = e.offsetX;
+//            var y = e.offsetY;
+//            if (event.touches) {
+//                x = event.touches[0].pageX;
+//                y = event.touches[0].pageY;
+//            }
+//            startPaint(x, y);
+//            if (onMouseDown != null && typeof(onMouseDown) === 'function') {
+//                onMouseDown(x, y);
+//            }
+
+//        });
+
+//        $(document).on('mouseup touchend', '#' + thisCanvasId, function (e) {
+//            endPaint();
+//            if (onMouseUp != null && typeof (onMouseUp) === 'function') {
+//                onMouseUp();
+//            }
+//        });
+
+//        $(document).on('mousemove touchmove', '#' + thisCanvasId, function (e) {
+
+//            var event = window.event;
+//            var x = e.offsetX;
+//            var y = e.offsetY;
+//            if (event.touches) {
+//                x = event.touches[0].pageX;
+//                y = event.touches[0].pageY;
+//            }
+//            draw(x,y);
+//            if (onMouseMove != null && typeof(onMouseMove) === 'function') {
+//                onMouseMove(x, y);
+//            }
+//        });
+//    };
+
+//    if (thisCanvasId)
+//        init();
+
+
+//    this.createNew = function (width, height, id) {
+//        var parent = $('#' + thisCanvasId).parent();
+//        $('<canvas id="' + id + '" width="' + width + '" height="' + height + '" style="border:1px #000 solid;cursor:pointer;background-color:black;"></canvas>').appendTo(parent);
+//        $('#' + thisCanvasId).remove();
+//        thisCanvasId = id;
+//        mouseDown = false;
+//        init();
+//    };
+
+//    this.getCurrentContext = function () {
+//        return context;
+//    };
+
+//    this.changeColor = function (color) {
+//        var ctx = this.getCurrentContext();
+//        ctx.strokeStyle = color;
+//    };
+
+//    var onMouseDown = null;
+//    Object.defineProperty(this, "onMouseDown", {
+//        get: function() {
+//            return onMouseDown;
+//        },
+//        set: function(value) {
+//            onMouseDown = value;
+//        }
+//    });
+//    var onMouseUp = null;
+//    Object.defineProperty(this, "onMouseUp", {
+//        get: function() {
+//            return onMouseUp;
+//        },
+//        set: function(value) {
+//            onMouseUp = value;
+//        }
+//    });
+
+//    var onMouseMove = null;
+//    Object.defineProperty(this, "onMouseMove", {
+//        get: function() {
+//            return onMouseMove;
+//        },
+//        set: function(value) {
+//            onMouseMove = value;
+//        }
+//    });
+//}
+
+(function ($) {
+
+    $.fn.Tablero = function (options) {
+        var mouseDown = false;
+        var canvas = null;
+        var context = null;
+        init.call(this, options);
+        return this;
+
+    };
+
+    var getCoordinates = function (e) {
+        var event = window.event;
+        var x = e.offsetX;
+        var y = e.offsetY;
+        if (event.touches) {
+            x = event.touches[0].pageX;
+            y = event.touches[0].pageY;
+        }
+        console.log(x);
+        console.log(y);
+        return { x: x, y: y };
+    }
+
+    var hookEvents = function () {
+        $(this).on('mousedown touchstart', function (e) {
+            var coords = getCoordinates(e);
+            startPaint(coords.x, coords.y);
+        });
+
+        $(this).on('mouseup touchend', function (e) {
+            endPaint();
+        });
+
+        $(this).on('mousemove touchmove', function (e) {
+            var coords = getCoordinates(e);
+            draw(coords.x, coords.y);
+        });
+
+    };
+
+    var init = function (options) {
+        var selector = $(this).attr('id');
+        canvas = document.getElementById(selector);
+        context = canvas.getContext("2d");
+        if (context)
+            context.strokeStyle = "White";
+        hookEvents();
+    };
 
     var startPaint = function (x, y) {
-        mouseDown = true;
-        canvas = document.getElementById(thisCanvasId);
-        context = canvas.getContext("2d");
+        this.mouseDown = true;
         context.lineWidth = 1;
         context.beginPath();
         context.moveTo(x, y);
     };
 
     var endPaint = function () {
-        mouseDown = false;
+        this.mouseDown = false;
     };
 
     var draw = function (x1, y1) {
         var x = Math.floor(x1) + 0.5;
         var y = Math.floor(y1) + 0.5;
         context.lineTo(x, y);
-        
-        if (mouseDown)
+        if (this.mouseDown)
             context.stroke();
     };
-    
-    var init = function () {
-        
-        canvas = document.getElementById(thisCanvasId);
-        
-        context = canvas.getContext("2d");
 
-        if (context)
-            context.strokeStyle = "White";
-
-        hookEvents();
-
-    };
-
-    this.startPaint = function(x, y) {
-        startPaint(x, y);
-    };
-
-    this.endPaint = function () {
-        endPaint();
-    };
-
-    this.draw = function (x, y) {
-        draw(x, y);
-    };
-
-    var hookEvents = function () {
-        $(document).on('mousedown touchstart', '#' + thisCanvasId, function (e) {
-            var event = window.event;
-            var x = e.offsetX;
-            var y = e.offsetY;
-            if (event.touches) {
-                x = event.touches[0].pageX;
-                y = event.touches[0].pageY;
-            }
-            startPaint(x, y);
-            if (onMouseDown != null && typeof(onMouseDown) === 'function') {
-                onMouseDown(x, y);
-            }
-                
-        });
-
-        $(document).on('mouseup touchend', '#' + thisCanvasId, function (e) {
-            endPaint();
-            if (onMouseUp != null && typeof (onMouseUp) === 'function') {
-                onMouseUp();
-            }
-        });
-
-        $(document).on('mousemove touchmove', '#' + thisCanvasId, function (e) {
-            //alert(e.pageX);
-            var event = window.event;
-            var x = e.offsetX;
-            var y = e.offsetY;
-            if (event.touches) {
-                x = event.touches[0].pageX;
-                y = event.touches[0].pageY;
-            }
-            draw(x,y);
-            if (onMouseMove != null && typeof(onMouseMove) === 'function') {
-                onMouseMove(x, y);
-            }
-        });
-    };
-
-    if (thisCanvasId)
-        init();
-
-    
-    this.createNew = function (width, height, id) {
-        var parent = $('#' + thisCanvasId).parent();
-        $('<canvas id="' + id + '" width="' + width + '" height="' + height + '" style="border:1px #000 solid;cursor:pointer;background-color:black;"></canvas>').appendTo(parent);
-        $('#' + thisCanvasId).remove();
-        thisCanvasId = id;
-        mouseDown = false;
-        init();
-    };
-
-    this.getCurrentContext = function () {
-        return context;
-    };
-
-    this.changeColor = function (color) {
-        var ctx = this.getCurrentContext();
-        ctx.strokeStyle = color;
-    };
-    
-    var onMouseDown = null;
-    Object.defineProperty(this, "onMouseDown", {
-        get: function() {
-            return onMouseDown;
-        },
-        set: function(value) {
-            onMouseDown = value;
-        }
-    });
-    var onMouseUp = null;
-    Object.defineProperty(this, "onMouseUp", {
-        get: function() {
-            return onMouseUp;
-        },
-        set: function(value) {
-            onMouseUp = value;
-        }
-    });
-    
-    var onMouseMove = null;
-    Object.defineProperty(this, "onMouseMove", {
-        get: function() {
-            return onMouseMove;
-        },
-        set: function(value) {
-            onMouseMove = value;
-        }
-    });
-}
+}(jQuery));

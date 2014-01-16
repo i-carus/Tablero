e01a0de811dfda757ac197cced6393390dabf3b6
i-carus <icarus@lablogoteca.com>
2014-01-16 01:33:30 -0500

    Trying to understand scoping on my JS code...

--- Scripts/Tablero.js
+++ Scripts/Tablero.js
@@ -1,8 +1,8 @@
 ﻿(function ($) {
 
     $.fn.Tablero = function (options) {
-        var mouseDown = false;
-        
+
+
         var canvas = null;
         var context = null;
 
@@ -13,10 +13,17 @@
             return context;
         };
 
-        init.call(this,options);
+        init.call(this, options);
 
+        this.lock = function () {
+            this.locked = true;
+        };
 
-        this.reset = function() {
+        this.unlock = function () {
+            this.locked = false;
+        };
+
+        this.reset = function () {
             window.location.reload();
         };
 
@@ -26,20 +33,22 @@
 
         ///These are all methods that are called remotely if the user provides
         this.startPaint = function (x, y) {
-           
-            startPaint(x, y);
+
+            startPaintRemote(x, y);
         };
 
         this.endPaint = function () {
-            endPaint();
+
+            endPaintRemote();
         };
 
         this.draw = function (x, y) {
-            draw(x, y);
+
+            drawRemote(x, y);
         };
 
         return this;
- 
+
     };
 
     var getCoordinates = function (e) {
@@ -52,25 +61,29 @@
         }
         return { x: x, y: y };
     };
-    
+
     var hookEvents = function (options) {
+        
+
         $(this).on('mousedown touchstart', function (e) {
+
             var coords = getCoordinates(e);
             startPaint(coords.x, coords.y);
             if (options.remoteStartPaint !== undefined) {
-                options.remoteStartPaint.call(this,coords.x, coords.y);
+                options.remoteStartPaint.call(this, coords.x, coords.y);
             }
         });
 
-        $(this).on('mouseup touchend', function (e) {
+        $(this).on('mouseup touchend',function (e) {
             endPaint();
             if (options.remoteEndPaint !== undefined) {
                 options.remoteEndPaint.call();
             }
         });
 
-        $(this).on('mousemove touchmove', function (e) {
+        $(this).on('mousemove touchmove',  function (e) {
             var coords = getCoordinates(e);
+            //console.log(this);
             draw(coords.x, coords.y);
             if (options.remoteDraw !== undefined) {
                 options.remoteDraw.call(this, coords.x, coords.y);
@@ -85,10 +98,11 @@
         context = canvas.getContext("2d");
         if (context)
             context.strokeStyle = "White";
-        hookEvents(options);
+        hookEvents.call(this, options);
     };
 
     var startPaint = function (x, y) {
+
         this.mouseDown = true;
         context.lineWidth = 1;
         context.beginPath();
@@ -97,15 +111,41 @@
 
     var endPaint = function () {
         this.mouseDown = false;
+
     };
 
     var draw = function (x1, y1) {
+        //console.log((this));
+
         var x = Math.floor(x1) + 0.5;
         var y = Math.floor(y1) + 0.5;
         context.lineTo(x, y);
         if (this.mouseDown)
             context.stroke();
+
     };
-    
+
+
+    var startPaintRemote = function (x, y) {
+        this.mouseDownRemote = true;
+        context.lineWidth = 1;
+        context.beginPath();
+        context.moveTo(x, y);
+    };
+
+    var endPaintRemote = function () {
+        this.mouseDownRemote = false;
+
+    };
+
+    var drawRemote = function (x1, y1) {
+        var x = Math.floor(x1) + 0.5;
+        var y = Math.floor(y1) + 0.5;
+        context.lineTo(x, y);
+        if (this.mouseDownRemote)
+            context.stroke();
+
+    };
+
 
 }(jQuery));
